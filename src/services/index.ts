f32edfc8e786994f0a8a3f53b21197ca32f4d852
ferco <ferco0@live.com>
2020-07-31 16:37:15 -0300

    create index file to facilitate services import

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export { CacheService } from "./cache";
+export { ContactVerificationService } from "./contact-verification";
+export { DataService } from "./data";
+export { EmailService } from "./email";
+export { JWTService } from "./jwt";
+export { SessionService } from "./session";
+export { StorageService } from "./storage";
