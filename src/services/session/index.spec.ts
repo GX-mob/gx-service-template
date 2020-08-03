00a705dbcede589f8cda068a8dedc9f4b43a9eda
ferco <ferco0@live.com>
2020-08-03 02:06:20 -0300

    implement jwt into session service

--- src/services/session/index.spec.ts
+++ src/services/session/index.spec.ts
@@ -4,49 +4,80 @@
  * @group unit/services/session
  */
 import { configureServiceTest } from "fastify-decorators/testing";
+import IORedis from "ioredis-mock";
 import { SessionService } from ".";
 import { DataService } from "../data";
+import { CacheService } from "../cache";
+
+const errorNotHavePrivateKey =
+  "should throw error due to not have the privateKey";
+let current: string;
+
+(jasmine as any).getEnv().addReporter({
+  specStarted: function (result: any) {
+    current = result.description;
+  },
+});
 
 describe("Service: Session", () => {
+  const processDotEnv = { ...process.env };
+
   let service: SessionService;
+
+  const userId = "foo";
+  const sid = "foo";
+  const ua = "test";
+  const ip = "127.0.0.1";
 
   const dataService = {
     users: {
-      get: jest.fn(),
+      get: jest.fn().mockResolvedValue({ _id: userId, groups: [1] }),
     },
     sessions: {
-      create: jest.fn(),
+      create: jest.fn().mockResolvedValue({
+        _id: sid,
+        userAgent: ua,
+        lastIp: ip,
+      }),
       get: jest.fn(),
       update: jest.fn(),
       remove: jest.fn(),
     },
   };
 
+  const cacheService = {
+    get: jest.fn(),
+    set: jest.fn(),
+  };
+
   beforeEach(async () => {
+    if (current === errorNotHavePrivateKey) {
+      const { AUTH_PRIVATE_KEY, ...newEnv } = process.env;
+
+      process.env = newEnv;
+    }
+
     service = configureServiceTest({
       service: SessionService,
-      mocks: [{ provide: DataService, useValue: dataService }],
+      mocks: [
+        { provide: DataService, useValue: dataService },
+        {
+          provide: CacheService,
+          useValue: cacheService,
+        },
+      ],
     });
   });
 
   afterEach(() => jest.restoreAllMocks());
 
   it("should create a session", async () => {
-    const userId = "foo";
-    const ua = "test";
-    const ip = "127.0.0.1";
-
-    dataService.users.get.mockResolvedValue({ _id: userId, groups: [1] });
-    dataService.sessions.create.mockResolvedValue({
-      userAgent: ua,
-      lastIp: ip,
-    });
-
-    const session = await service.create(userId, {
+    const { token, session } = await service.create(userId, {
       ua,
       ip,
     });
 
+    expect(typeof token).toBe("string");
     expect(session.userAgent === ua && session.lastIp === ip).toBeTruthy();
   });
 
@@ -68,5 +99,64 @@ describe("Service: Session", () => {
     const check = await service.get("foo");
 
     expect(check).toBe(null);
+  });
+
+  it(errorNotHavePrivateKey, async () => {
+    await expect(
+      service.create(userId, {
+        ua,
+        ip,
+      })
+    ).rejects.toThrow(
+      "This service does not have the private key, isn't allowed to sign an authentication token"
+    );
+
+    process.env = processDotEnv;
+  });
+
+  it("should verify a token", async () => {
+    cacheService.get.mockResolvedValue(null);
+    cacheService.set.mockResolvedValue("OK");
+
+    const { token } = await service.create(userId, {
+      ua,
+      ip,
+    });
+
+    dataService.sessions.get.mockResolvedValue({ active: true });
+
+    const tokenDecoded = await service.verify(token);
+
+    expect(tokenDecoded).toMatchObject({
+      sid,
+      uid: userId,
+    });
+
+    cacheService.get.mockResolvedValue(tokenDecoded);
+
+    const fromCache = await service.verify(token);
+
+    expect(fromCache).toMatchObject({
+      sid,
+      uid: userId,
+    });
+  });
+
+  it("should throw an error due to deactivated or non existent session", async () => {
+    cacheService.get.mockResolvedValue(null);
+    cacheService.set.mockResolvedValue("OK");
+
+    const { token } = await service.create(userId, {
+      ua,
+      ip,
+    });
+
+    dataService.sessions.get.mockResolvedValue(null);
+
+    await expect(service.verify(token)).rejects.toThrow("Session deactivated");
+
+    dataService.sessions.get.mockResolvedValue({ active: false });
+
+    await expect(service.verify(token)).rejects.toThrow("Session deactivated");
   });
 });
--- src/services/session/index.ts
+++ src/services/session/index.ts
@@ -1,6 +1,13 @@
 import { Service, Inject } from "fastify-decorators";
 import { Types } from "mongoose";
+import { promisify } from "util";
+import jwt from "jsonwebtoken";
 import { DataService } from "../data";
+import { CacheService } from "../cache";
+import { Session } from "../../models/session";
+
+const verify = promisify(jwt.verify);
+const sign = promisify(jwt.sign);
 
 /**
  * Session data
@@ -18,25 +25,59 @@ type SessionInfo = {
 
 @Service()
 export class SessionService {
+  private tokenNamespace = "token";
+  private keyid: string;
+  private publicKey: string;
+  private privateKey: string;
+
   @Inject(DataService)
   private data!: DataService;
 
+  @Inject(CacheService)
+  private cache!: CacheService;
+
+  constructor() {
+    this.keyid = process.env.AUTH_KID;
+    this.publicKey = process.env.AUTH_PUBLIC_KEY;
+    this.privateKey = process.env.AUTH_PRIVATE_KEY;
+  }
+
   /**
    * @param user_id
-   * @param session_data
-   * @return
-   * @constructs SessionModel
+   * @param session_data Object with userAgent and user IP
+   * @return {Object} { token: string, session: SessionModel }
    */
-  async create(user_id: Types.ObjectId | string, session_data: SessionInfo) {
+  async create(
+    user_id: Types.ObjectId | string,
+    session_data: SessionInfo
+  ): Promise<{ token: string; session: Session }> {
     const { _id, groups } = await this.data.users.get({
       _id: user_id,
     });
 
-    return this.data.sessions.create({
+    const session = await this.data.sessions.create({
       uid: _id,
       groups,
       userAgent: session_data.ua,
       lastIp: session_data.ip,
+    });
+
+    const token = await this.signToken({ sid: session._id, uid: _id });
+
+    return { token, session };
+  }
+
+  private signToken(data: any): Promise<string> {
+    if (!this.privateKey) {
+      throw new Error(
+        "This service does not have the private key," +
+          " isn't allowed to sign an authentication token"
+      );
+    }
+
+    return sign({ ...data }, this.privateKey, {
+      algorithm: "ES256",
+      keyid: this.keyid,
     });
   }
 
@@ -50,5 +91,35 @@ export class SessionService {
 
   async delete(session_id: Types.ObjectId | string) {
     await this.data.sessions.remove({ _id: session_id });
+  }
+
+  /**
+   * Verify a token
+   * @param token
+   * @returns session data
+   */
+  async verify(token: string): Promise<any> {
+    const cache = await this.cache.get(this.tokenNamespace, token);
+
+    if (cache) {
+      await this.checkState(cache.sid);
+      return cache;
+    }
+
+    const data = await verify(token, this.publicKey, { algorithms: ["ES256"] });
+
+    await this.checkState(data.sid);
+
+    this.cache.set(this.tokenNamespace, token, data);
+
+    return data;
+  }
+
+  private async checkState(session_id: Types.ObjectId | string) {
+    const sessionData = await this.get(session_id);
+
+    if (!sessionData || !sessionData.active) {
+      throw new Error("Session deactivated");
+    }
   }
 }
