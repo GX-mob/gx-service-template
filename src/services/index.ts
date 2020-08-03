30c7d685220684528c8165db8f3aa6ea32aedeec
ferco <ferco0@live.com>
2020-08-03 02:12:57 -0300

    remove jwt from services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,5 @@ export { CacheService } from "./cache";
 export { ContactVerificationService } from "./contact-verification";
 export { DataService } from "./data";
 export { EmailService } from "./email";
-export { JWTService } from "./jwt";
 export { SessionService } from "./session";
 export { StorageService } from "./storage";
